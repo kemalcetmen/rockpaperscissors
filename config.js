40b4826700077592488e381ca59dc0da4ab02fd6
kemcet <kemalcetmensm@gmail.com>
2022-07-10 14:59:55 +0300

    make config visible

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAQmNbWHlPprk1JcZ_7NJ8NWK3VAnaCXMU",
+    authDomain: "rockpaperscissors-4be6d.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-4be6d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rockpaperscissors-4be6d",
+    storageBucket: "rockpaperscissors-4be6d.appspot.com",
+    messagingSenderId: "11118609562",
+    appId: "1:11118609562:web:ee46ad985876d2f171cb23",
+    measurementId: "G-H21NE5NBY4"
+};
+
+export default config
